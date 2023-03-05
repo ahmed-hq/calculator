ca27675f3f185f9f304fa76ca6875023562fab5d
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-03-05 14:35:59 +0200

    Finish typing the first number

--- script.js
+++ script.js
@@ -1,11 +1,74 @@
-let firstNum = [];
-let secNum = [];
-let operator = [];
+let firstNumArr = [];
+let firstNum = firstNumArr.join("");
+let secNumArr = [];
+let secNum = Number(secNumArr.join(""));
+let operator;
 let result = 0;
 
+let resultText = document.querySelector(".result");
+let firstNumText = document.querySelector(".first-num");
+let secNumText = document.querySelector(".sec-num");
+let operatorText = document.querySelector(".operator");
+let numOne = document.getElementById("num-1");
+let numTwo = document.querySelector("#num-2");
+let numThree = document.querySelector("#num-3");
+let numFour = document.querySelector("#num-4");
+let numFive = document.querySelector("#num-5");
+let numSix = document.querySelector("#num-6");
+let numSeven = document.querySelector("#num-7");
+let numEight = document.querySelector("#num-8");
+let numNine = document.querySelector("#num-9");
+let clearButton = document.querySelector(".ce-button")
+
+clearButton.addEventListener("click", clear)
+
+numOne.addEventListener("click", () => {
+  inputNum(1);
+});
+numTwo.addEventListener("click", () => {
+  inputNum(2);
+});
+numThree.addEventListener("click", () => {
+  inputNum(3);
+});
+numFour.addEventListener("click", () => {
+  inputNum(4);
+});
+numFive.addEventListener("click", () => {
+  inputNum(5);
+});
+numSix.addEventListener("click", () => {
+  inputNum(6);
+});
+numSeven.addEventListener("click", () => {
+  inputNum(7);
+});
+numEight.addEventListener("click", () => {
+  inputNum(8);
+});
+numNine.addEventListener("click", () => {
+  inputNum(9);
+});
 
 
-let firstNumText = document.querySelector(".first-num").innerText = `${firstNum}`;
-let operatorText = document.querySelector(".operator").innerText = `${operator}`;
-let secNumText = document.querySelector(".sec-num").innerText = `${secNum}`;
-let resultText = document.querySelector(".result").innerText = `${result}`
+function resetFirstNum(){
+    firstNumText.innerText = `${firstNumArr.join("")}`;
+}
+
+function inputNum(num) {
+  firstNumArr.push(num);
+  resetFirstNum()
+}
+
+function clear() {
+  firstNumArr = [];
+  secNumArr = [];
+  result = 0;
+  resetFirstNum()
+  
+}
+
+
+// .innerText = `${operator}`;
+// .innerText = `${secNum}`;
+// .innerText = `${result}`
