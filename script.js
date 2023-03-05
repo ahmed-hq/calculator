9d3ec340aa7526f656aeb910fc44f501f52c54e9
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-03-05 08:31:51 +0200

    Setting variables

---
+++ script.js
@@ -0,0 +1,11 @@
+let firstNum = [];
+let secNum = [];
+let operator = [];
+let result = 0;
+
+
+
+let firstNumText = document.querySelector(".first-num").innerText = `${firstNum}`;
+let operatorText = document.querySelector(".operator").innerText = `${operator}`;
+let secNumText = document.querySelector(".sec-num").innerText = `${secNum}`;
+let resultText = document.querySelector(".result").innerText = `${result}`
